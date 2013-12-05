3f3e8dc5616405c76f5efc988ade7a2d5bc2e85e
David Fisher <fisherds@rose-hulman.edu>
2013-12-05 08:57:45 -0500

    Updating API to current naming system
    Using the DELETE method for delete and updating paths and names

--- Solution/Backend/fisherds-movie-quotes/movie_quotes_api.py
+++ Solution/Backend/fisherds-movie-quotes/movie_quotes_api.py
@@ -1,6 +1,5 @@
 """ Movie quotes API"""
 
-
 import endpoints
 from protorpc import remote
 
@@ -10,25 +9,25 @@ from models import MovieQuote
 class MovieQuotesApi(remote.Service):
     """ Class which defines moviesquotes API"""
     
-    @MovieQuote.method(path='quote', http_method='POST', name='quote.insert')
+    @MovieQuote.method(path='quote/insert', http_method='POST', name='quote.insert')
     def quote_insert(self, a_quote):
         """ Insert a quote. """
         a_quote.put()
         return a_quote
     
-    @MovieQuote.query_method(query_fields=('limit', 'order', 'pageToken'), path='quotes', http_method='GET', name='quotes.list')
+    @MovieQuote.query_method(query_fields=('limit', 'order', 'pageToken'), path='quote/list', http_method='GET', name='quote.list')
     def quotes_list(self, query):
         """ Return a list of quotes. """
         return query
     
-    @MovieQuote.method(request_fields=('id',), path='quote/delete/{id}', http_method='GET', name='quote.delete')
+    @MovieQuote.method(request_fields=('id',), path='quote/delete/{id}', http_method='DELETE', name='quote.delete')
     def quote_delete(self, a_quote):
         """ Delete a quote. """
         if not a_quote.from_datastore:
-            raise endpoints.NotFoundException("Quote not found.")
+            raise endpoints.NotFoundException("Quote not found.  Nothing deleted.")
         a_quote.key.delete()
         return MovieQuote(quote="deleted")
 
-APPLICATION = endpoints.api_server([MovieQuotesApi], restricted=False)    
+app = endpoints.api_server([MovieQuotesApi], restricted=False)    
 
     
